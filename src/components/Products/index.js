f8c19fb15a5b4a42b72df0a1f478faf4cfe197e6
amandagdev <it.amandag@gmail.com>
2020-11-24 00:29:33 -0300

    card product responsive

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -6,19 +6,23 @@ import Button from '../Button'
 export default function Products ({ item }) {
   return (
     <>
-      <div className="product">
-        <div className="product-image">
-         <img src={Cake} alt="imagem" /> 
+      <div className='product'>
+        <div className='product-image'>
+          <img src={Cake} alt='imagem' />
         </div>
-        <div className="product-title">
+        <div className='product-title'>
           <p>{item.nome}</p>
           <span>R$ {item.valor}</span>
         </div>
-        <div className="product-description">
+        <div className='product-description'>
           <p>{item.descricao}</p>
         </div>
-        <div className="product-button">
-          <Button className="button-product">Adicionar ao carrinho</Button>
+        <div className='product-value'>
+          <span>R$ {item.valor}</span>
+        </div>
+
+        <div className='product-button'>
+          <Button className='button-product'>Adicionar ao carrinho</Button>
         </div>
       </div>
     </>
