4123b18d853d79b850922792ab4feb3b56eeb6f5
amandagdev <it.amandag@gmail.com>
2020-11-23 21:13:12 -0300

    feat(home): add products

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import './styles.css'
 
 import PageFooter from '../../components/PageFooter'
 import PageHeader from '../../components/PageHeader'
 
-function Home() {
-    return (
-        <>
-            <PageHeader/>
-            <PageFooter/>
-        </>
-    )
+function Home () {
+  return (
+    <>
+      <PageHeader />
+      <div className='home'>
+        <h2>Produtos</h2>
+        <div className='home-produtos'></div>
+      </div>
+      <PageFooter />
+    </>
+  )
 }
 
 export default Home
