322ddef1d3275a2b3556949fb84ca6d6b3dafcea
amandagdev <it.amandag@gmail.com>
2020-11-24 23:07:29 -0300

    style(*): cart style created

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './styles.css'
 import Cake from '../../assets/images/bolos.jpg'
+import { MdRemoveCircleOutline, MdAddCircleOutline } from 'react-icons/md'
+import Button from '../Button'
 
 function Cart ({ item }) {
   return (
@@ -11,7 +13,22 @@ function Cart ({ item }) {
       <div className="cart-product-values">
         <p className="value-name">{item.nome}</p>
         <p className="value-description">{item.descricao}</p>
-        <span className="valeu">{item.valor}</span>
+        <div className="cart-count">
+          <Button type="button" className="button-count">
+            <MdRemoveCircleOutline
+              size={20}
+              color="var(--color-border)"
+            />
+          </Button>
+          <input type="text" value={item.quantity} readOnly />
+          <Button
+            type="button"
+            className="button-count"
+          >
+            <MdAddCircleOutline size={20} color="var(--color-border)" />
+          </Button>
+        </div>
+        <span className="valeu">R$ {item.valor}</span>
       </div>
     </div>
   )
